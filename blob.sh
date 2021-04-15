7d59418c1caecf7f95dbbcd8c96761b211618228
Cenon Ebora <cenon4dno@gmail.com>
2021-04-16 01:51:08 +0800

    Delete blob.sh

--- blob.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-blobStorageLink='https://bloblab01.blob.core.windows.net/recordings'
-blobStorageDelimiter='?'
-blobStorageSAS='sp=racwdl&st=2021-04-15T15:26:48Z&se=2022-04-17T23:26:48Z&spr=https&sv=2020-02-10&sr=c&sig=DW%2BA2uL3Nz5ItxRWktjRgNf4BWGTediFzRMpIKgJnCs%3D'
