421b97071bb54bebfd41663befa2b5013ff6b92a
Cenon Dinno Ebora III <ceeborai@microsoft.com>
2021-04-16 13:45:12 +0800

    update for powershell script

--- powershell/main.ps1
+++ powershell/main.ps1
@@ -15,9 +15,9 @@ $history = 'history'
 
 # Sync Files
 $command="azcopy sync '$recordingsDirectory' '$blobStorageLink$blobStorageDelimiter$blobStorageSAS'"
-Invoke-Expression -Command $command | Out-File -FilePath "$($logDirectory)$($run)$($date).log"
+Invoke-Expression -Command $command | Add-Content -Path "$($logDirectory)$($run)$($date).log"
 
-# Write to videourl
+# Write to fileurl
 Out-File -FilePath "$($logDirectory)$($fileurl)$($date).log"
 Get-ChildItem $recordingsDirectory -recurse | ForEach-Object {
      $file = $_.Name
@@ -25,6 +25,6 @@ Get-ChildItem $recordingsDirectory -recurse | ForEach-Object {
 }
 
 # Write to history
-Write-Output "$datetime Synced '$recordingsDirectory' directory" | Out-File -FilePath "$($logDirectory)$($history).log"
+Write-Output "$datetime Synced '$recordingsDirectory' directory" | Add-Content -Path "$($logDirectory)$($history).log"
 
 Write-Output 'End Application'
